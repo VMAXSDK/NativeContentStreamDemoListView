70aa22e8e6c1838150d837b417ea941452d9d8c3
prathameshs <prathamesh.s@vmax.com>
2017-04-21 15:23:02 +0530

    Final Commit

--- app/src/main/java/com/vmax/demo/nativeCSList/listviewadapter.java
+++ app/src/main/java/com/vmax/demo/nativeCSList/listviewadapter.java
@@ -45,7 +45,7 @@ public class listviewadapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         View vw;
         TextView textView;
-        if (i==4)
+        if (i==4&&theAd!=null)
         {
             /** Ad Will be displayed on the fourth position in your list*/
             vw=theAd;
